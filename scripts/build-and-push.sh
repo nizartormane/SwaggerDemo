2c8077aca60c5d322a3c8b90079e504768a7a9ee
nizartormane <Nizar.tormanee@gmail.com>
2025-05-10 17:03:15 +0100

    new com

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -8,21 +8,21 @@ TAG_NAME="${GITHUB_REF#refs/tags/}"
 for SERVICE in $MODIFIED_SERVICES; do
   case $SERVICE in
     v-auth)
-      docker build -t authserver -f src/SwaggerDemo.AuthServer/Dockerfile .
+      docker build -t authserver -f ./src/SwaggerDemo.AuthServer/Dockerfile .
       docker tag authserver $ECR_REPO:authserver-$TAG_NAME
       docker tag authserver $ECR_REPO:authserver-latest
       docker push $ECR_REPO:authserver-$TAG_NAME
       docker push $ECR_REPO:authserver-latest
       ;;
     v-dbmigrator)
-      docker build -t dbmigrator -f src/SwaggerDemo.DbMigrator/Dockerfile .
+      docker build -t dbmigrator -f ./src/SwaggerDemo.DbMigrator/Dockerfile .
       docker tag dbmigrator $ECR_REPO:dbmigrator-$TAG_NAME
       docker tag dbmigrator $ECR_REPO:dbmigrator-latest
       docker push $ECR_REPO:dbmigrator-$TAG_NAME
       docker push $ECR_REPO:dbmigrator-latest
       ;;
     v-http)
-      docker build -t httpapi -f src/SwaggerDemo.HttpApi.Host/Dockerfile .
+      docker build -t httpapi -f ./src/SwaggerDemo.HttpApi.Host/Dockerfile .
       docker tag httpapi $ECR_REPO:httpapi-$TAG_NAME
       docker tag httpapi $ECR_REPO:httpapi-latest
       docker push $ECR_REPO:httpapi-$TAG_NAME
